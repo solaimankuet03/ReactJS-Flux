c20d137e5f663d13fd18be5e123c570a09a60db4
solaiman ali <solaimankuet03@gmail.com>
2021-04-18 14:54:52 +0600

    Forms edit, add, validation

--- ps-flux/src/components/AboutPage.js
+++ ps-flux/src/components/AboutPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import {Prompt} from 'react-router-dom';
 class AboutPage extends React.Component{
     render(){
         return(
         <React.Fragment>
         <h1>About Page</h1>
+        <Prompt when={true} message="Are you sure you want to leave?"></Prompt>
         <p>This is the about page</p>
         </React.Fragment>    
         );
